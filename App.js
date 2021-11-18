67062e2b2312a925ff92f5afb2ba19f07d22a256
byamb4 <byamba4life@gmail.com>
2021-11-18 23:00:20 +0800

    [update] Removed unncessary function :whale:

--- App.js
+++ App.js
@@ -19,21 +19,13 @@ new Vue({
     ],
   },
   methods: {
-    async changeUser() {
-      const res = await fetch("https://randomuser.me/api/");
-      const { results } = await res.json();
-      this.name = results[0].name.first;
-      this.email = results[0].email;
-      this.picture = results[0].picture.large;
-      this.gender = results[0].gender;
-    },
     addAccount() {
       this.accounts.push({
         name: this.name,
         email: this.email,
       });
-      this.name = ""
-      this.email = ""
+      this.name = "";
+      this.email = "";
     },
   },
 });
